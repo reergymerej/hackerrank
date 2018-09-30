7f4916060cb3023e8ae14b5140b8755789b77dbf
Jeremy Greer <jex.grizzle@gmail.com>
2018-09-30 12:43:03 -0400

    Solve array left rotation

--- challenges/array-left-rotation/solution.js
+++ challenges/array-left-rotation/solution.js
@@ -1,4 +1,7 @@
+// https://www.hackerrank.com/challenges/array-left-rotation/problem
 'use strict';
+
+const fs = require('fs');
 
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
@@ -22,9 +25,17 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-
+function rotateLeft(arr, count) {
+  while (count--) {
+    arr.push(arr.shift())
+  }
+  return arr
+}
 
 function main() {
+    process.stdout.write(process.env.OUTPUT_PATH)
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
     const nd = readLine().split(' ');
 
     const n = parseInt(nd[0], 10);
@@ -32,4 +43,10 @@ function main() {
     const d = parseInt(nd[1], 10);
 
     const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = rotateLeft(a, d)
+
+    ws.write(result.join(' ') + "\n");
+
+    ws.end();
 }
