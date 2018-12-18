407ace89aa7ece5df81e4ffabe16c1bf3b82d708
Jeremy Greer <jex.grizzle@gmail.com>
2018-12-18 10:45:48 -0500

    filter array

--- challenges/fp-filter-array/solution.exs
+++ challenges/fp-filter-array/solution.exs
@@ -7,3 +7,13 @@ end
 # line 1 has threshold
 # lines 2.. have numbers
 # print lines under threshold
+
+lines = String.split(IO.read(:stdio, :all))
+threshold = String.to_integer(hd(lines))
+
+Enum.each(tl(lines), fn(x) ->
+  int = String.to_integer(x)
+  if (int < threshold) do
+    IO.puts(int)
+  end
+end)
