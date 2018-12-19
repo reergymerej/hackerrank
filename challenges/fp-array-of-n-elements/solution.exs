cf07936f17c2380a84d9d3150af1dad7e5a4ce7f
Jeremy Greer <jex.grizzle@gmail.com>
2018-12-19 10:39:49 -0500

    Solve array of n elements

--- challenges/fp-array-of-n-elements/solution.exs
+++ challenges/fp-array-of-n-elements/solution.exs
@@ -1,7 +1,7 @@
-defmodule Solution do
-end
-
 lines = String.split(IO.read(:stdio, :all))
 count = String.to_integer(hd(lines))
+range = 0..(count - 1)
+joined = Enum.join(range, ", ")
+result = "[#{joined}]"
 
-IO.puts(0..(count - 1))
+IO.puts(result)
