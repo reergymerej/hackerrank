cac133a276e8178599c42ec85df94941855c5e64
Jeremy Greer <jeremy.greer@engageonq.com>
2018-12-22 21:44:14 -0500

    Fix and clean up odd elements

--- challenges/fp-sum-of-odd-elements/solution.exs
+++ challenges/fp-sum-of-odd-elements/solution.exs
@@ -1,15 +1,11 @@
 defmodule Solution do
   def is_odd(x) do
-    rem(x, 2) === 1
+    rem(x, 2) !== 0
   end
 end
 
-lines = String.split(IO.read(:stdio, :all))
-
-nums = Enum.map(lines, fn(x) -> String.to_integer(x) end)
-
-odds = Enum.filter(nums, fn(x) -> Solution.is_odd(x) end)
-
-result = Enum.reduce(odds, 0, fn(x, acc) -> x + acc end)
-
-IO.puts(result)
+String.split(IO.read(:stdio, :all))
+  |> Enum.map(fn(x) -> String.to_integer(x) end)
+  |> Enum.filter(fn(x) -> Solution.is_odd(x) end)
+  |> Enum.reduce(0, fn(x, acc) -> x + acc end)
+  |> IO.puts()
