46ff815805592d61ac29d3dccdf31591f243e0bd
Jeremy Greer <jeremy.greer@engageonq.com>
2018-12-24 13:23:58 -0500

    Add pow function

--- challenges/eval-ex/solution.exs
+++ challenges/eval-ex/solution.exs
@@ -1,11 +1,17 @@
 # 1 + x + x^2/2! + x^3/3! + x^4/4!...
 
 defmodule Solution do
-  def factorial(x) when x === 1, do: x
+  def factorial(x) when (x === 1), do: x
 
-  def factorial(x) when x > 1, do: x + factorial(x - 1)
+  def factorial(x) when (x > 1), do: x + factorial(x - 1)
 
+  def pow(_, y) when (y === 0), do: 1
 
+  def pow(x, y) when (y === 1), do: x
+
+  def pow(x, y) when (y > 0), do: x * pow(x, y - 1)
+
+  def pow(x, y) when (y < 0), do: 1 / pow(x, abs(y))
 end
 
-IO.puts(Solution.factorial(3))
+IO.puts(Solution.pow(2, -3))
