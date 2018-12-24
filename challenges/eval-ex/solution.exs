e9a26cf8bb80da2ce4c76e0df38d624612055867
Jeremy Greer <jeremy.greer@engageonq.com>
2018-12-24 12:53:12 -0500

    Handle negative values

--- challenges/eval-ex/solution.exs
+++ challenges/eval-ex/solution.exs
@@ -3,7 +3,9 @@
 defmodule Solution do
   def factorial(x) when x === 1, do: x
 
-  def factorial(x), do: x + factorial(x - 1)
+  def factorial(x) when x > 1, do: x + factorial(x - 1)
+
+
 end
 
 IO.puts(Solution.factorial(3))
