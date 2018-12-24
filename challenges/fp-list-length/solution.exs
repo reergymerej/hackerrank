81815d0f72dc32141a2c7409b2a9505a2dfd5dbb
Jeremy Greer <jeremy.greer@engageonq.com>
2018-12-23 19:21:21 -0500

    Solve with recursive fn, head/tail

--- challenges/fp-list-length/solution.exs
+++ challenges/fp-list-length/solution.exs
@@ -1,6 +1,15 @@
-# count how many input lines without using count, size, or length
+defmodule Solution do
+  def get_count(list, count) do
+    case list  do
+      [h|t] ->
+        get_count(t, count + 1)
+      _ ->
+        count
+    end
+  end
+end
 
 IO.read(:stdio, :all)
   |> String.split()
-  |> length()
+  |> Solution.get_count(0)
   |> IO.puts()
